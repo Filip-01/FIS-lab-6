bbf3a50383c153ccff13fec3648bcb8fc08aa456
Jorge Hernández Toledo <62747754+JorgeHerTo@users.noreply.github.com>
2021-04-20 13:41:36 +0100

    Update prueba.cc
    He pueso que doy pena porque es verdad

--- prueba.cc
+++ prueba.cc
@@ -4,6 +4,7 @@ int main(void) {
     std::cout << "Hola xd\n"; //holi
     std::cout << "Soy Pablo" << std::endl;
     std::cout << "Hola xd\n"
-              << "Pesao\n";
+              << "Pesao\n"
+              << "Doy pena chicos att hache";
     return 0;
 }
