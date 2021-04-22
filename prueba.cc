8ef71ebcfcd5ca690a9961910e0890829f0843f7
Felipe Gómez <alu0101315713@ull.edu.es>
2021-04-22 14:35:55 +0100

    pa ver si está brian
    estará?

--- prueba.cc
+++ prueba.cc
@@ -4,6 +4,7 @@ int main(void) {
     std::cout << "Hola xd\n"; //holi
     std::cout << "Soy Pablo" << std::endl;
     std::cout << "Hola xd\n"
-              << "Pesao\n";
+              << "Pesao\n"
+              << "Brian está?\n";
     return 0;
 }
