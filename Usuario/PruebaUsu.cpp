268157059ffe2c418e6c2a62d9849769ddb180c1
Alejandro García Perdomo <alu0101312101@ull.edu.es>
2021-04-27 10:17:51 +0100

    Update de PruebaUsu.cpp
    Para probar si se escriben las contraseñas o no, y si lo hacen si se comprueban bien.

--- Usuario/PruebaUsu.cpp
+++ Usuario/PruebaUsu.cpp
@@ -14,10 +14,26 @@ int main(int argc, char const *argv[])  {
     std::cout << "Introduzca nuevo nombre.\n";
     std::cin >> nombre;
 
+    std::string passwd;
+    std::cout << "Introduzca la contraseña.\n";
+    std:.cin >> passwd;
+  
+    usuario.SetPasswd(passwd);
+  
+    usuario.WritePasswd(passwd);
+  
     usuario.WriteName(nombre);
     
+    std::cout << "Introduzca contraseña errónea o correcta";
+    std::cin >> passwd;
     if (usuario.CheckUser(nombre))  {
       std::cout << "ok\n";
+      if (usuario.CheckPasswd(passwd))  {
+        std::cout << "contraseña ok";
+      }
+      else {
+        std:.cout << "Contraseña no ok"
+      }
     }
     else {
       std::cout << "no ok\n";
