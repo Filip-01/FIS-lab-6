a5f3d17a548a0ff58ccaa54d49c7659978aa57ea
Pablo Pais Ficret <64638993+Pablo400@users.noreply.github.com>
2021-05-02 11:54:24 -0700

    Update programa principal
    He cambiado algunas cosas como: añadir funciones para el registro y el inicio de sesión, y un submenu que contiene otras funcionalidades cuando el usuario inicie sesión.

--- Programa principal/main.cc
+++ Programa principal/main.cc
@@ -5,19 +5,23 @@
 #include <iostream>
 #include "../Usuario/Usuario.h"
 
-int menu();
-void opciones();
+int logMenu();
+void logmenuOptions();
+int mainMenu();
+void mainmenuOptions();
 void separation();
 void userRegister();
-void userLogin();
+bool userLogin();
 
-int main()
-{
-    opciones();
+
+int main() {
+    std::cout << "\nBienvenido a PapaGayo Store" << std::endl;
+    logmenuOptions();
     return 0;
 }
 
-int menu() {
+
+int logMenu() {
     int selection;
     separation();
     std::cout << "[1] ¿No tienes cuenta? ¡REGÍSTRATE YA!" << std::endl;
@@ -28,27 +32,30 @@ int menu() {
     return selection;
 }
 
+
 void separation() {
     std::cout << "________________________________________" << std::endl;
     std::cout << std::endl;
 }
 
-void opciones() {
+
+void logmenuOptions() {
     int option = 0;
-    std::cout << "Bienvenido a PapaGayo Store" << std::endl;
     Usuario user;
     std::string name;
     std::string aux;
     std::string passwd;
-    while (option != 3) {
-        option = menu();
+    while (option != 3 && option != 2) {
+        option = logMenu();
         switch (option) {
-            case 1:  {
+            case 1: 
                 userRegister();
             break;
-            }
+
             case 2:
-                userLogin();
+                if (userLogin()) {
+                    mainmenuOptions();
+                }
             break; 
         
             case 3: 
@@ -62,8 +69,56 @@ void opciones() {
             break;
         }
     }
-
 }
+
+int mainMenu() {
+    int selection;
+    separation();
+    std::cout << "[1] Buscar productos" << std::endl;
+    std::cout << "[2] Publicar un producto" << std::endl;
+    std::cout << "[3] Comprar un producto" << std::endl; 
+    std::cout << "[4] Carrito de compra" << std::endl;  
+    std::cout << "[5] SALIR" << std::endl;
+    std::cout << "Seleccione una opcion -> ";
+    std::cin >> selection;
+    return selection;
+}
+
+
+void mainmenuOptions() {
+    int option = 0;
+    while (option != 5) {
+        option = mainMenu();
+        switch (option) {
+            case 1: 
+                separation();
+            break;
+
+            case 2:
+                separation();
+            break;
+                
+            case 3:
+                separation();
+            break;
+
+            case 4:
+                separation();
+            break;
+
+            case 5: 
+                separation();
+                std::cout << "Vuelva pronto." << std::endl;
+            break; 
+        
+            default:
+                separation();
+                std::cout << "Por favor, introduzca una opción válida" << std::endl;
+            break;
+        }
+    }    
+} 
+
 
 void userRegister() {
     separation();
@@ -71,8 +126,7 @@ void userRegister() {
     std::string name;
     std::string aux;
     std::string passwd;
-    bool end = true;
-    do  {
+    
         std::cout << "Introduce un nombre de usuario: " << std::endl;
         std::cin >> name;
         if (!user.CheckUser(name))  {
@@ -89,18 +143,17 @@ void userRegister() {
         
         user.SetPasswd(passwd);
         user.AddName();
-        user.AddPasswd();
+        std::cout << "Usuario añadido." << std::endl;
       }
 
       else  {
           std::cout << "El nombre de usuario ya existe." << std::endl;
-          end = false;
+          logmenuOptions();
       }
-    } while (!end);
-    std::cout << "Usuario añadido." << std::endl;
 }
 
-void userLogin() {
+
+bool userLogin() {
     Usuario user;
     std::string name;
     std::string aux;
@@ -114,8 +167,14 @@ void userLogin() {
     std::cin >> passwd;
     user.SetPasswd(passwd);
                 
-    if (user.CheckUser(name) && user.CheckPasswd(passwd))
+    if (user.CheckUser(name) && user.CheckPasswd(passwd)) {
         std::cout << "¡Login correcto!" << std::endl;
-    else
-        std::cout << "Login incorrecto." << std::endl;
+        return true;
+    }
+    else {
+        std::cout << "Login incorrecto." << std::endl;  
+        return false;   
+    }     
 }
+
+
